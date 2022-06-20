411a1e827c43399b72f8c1053d9f60c6f453d38b
mitanshu patel <mitanshu.patel@bacancy.com>
2022-06-20 23:43:54 +0530

    footer added

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import React from "react";
 
 function Footer() {
@@ -6,12 +6,50 @@ function Footer() {
     <footer
       style={{
         backgroundColor: "lightgray",
-        minHeight: "400px",
+        minHeight: "300px",
         minWidth: "100%",
       }}
     >
-      <Box sx={{ maxWidth: "1500px", margin: "30px auto" }}>
-        <p>copyright</p>
+      <Box sx={{ maxWidth: "1500px", margin: "0 auto" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={3} columns={{ xs: 1 }}>
+            <Box sx={{ px: 2 }}>
+              <h3>About Us</h3>
+              <p>Contact Us</p>
+              <p>About Us</p>
+              <p>Wholesale</p>
+              <p>Stories</p>
+              <p>Information</p>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} columns={{ xs: 1 }}>
+            <Box sx={{ px: 2 }}>
+              <h3>Help</h3>
+              <p>Payments</p>
+              <p>Shipping</p>
+              <p>Cancellation & Returns </p>
+              <p>FAQ</p>
+              <p>Report</p>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} columns={{ xs: 1 }}>
+            <Box sx={{ px: 2 }}>
+              <h3>Policy</h3>
+              <p>Return Policy</p>
+              <p>Terms Of Use</p>
+              <p>Security</p>
+              <p>Privacy</p>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} columns={{ xs: 1 }}>
+            <Box sx={{ px: 2 }}>
+              <h3>Contact Us</h3>
+              <p>Email:xyz@gamil.com</p>
+              <p>Phone:+91-123-456-7890</p>
+              <p>Address:ABC Road,Bangalore</p>
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
     </footer>
   );
